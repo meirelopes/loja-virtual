cd6bdfccb16fa7707799f02ce1bf413066e8c33f
Jucemeire Lopes <jucemeirelopes@gmail.com>
2023-10-01 15:08:39 -0300

    iniciando

---
+++ backend/src/main/java/com/lopes/backend/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.lopes.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+    @GetMapping("/")
+    public String hello() {
+
+        return "Olá Mundo! - " + new Date();
+
+    }
+
+}
